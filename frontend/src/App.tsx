64a2d53aca769d53bdeb3850bcc037cfd7e26f14
Nikhil Mummaneni <your_email@example.com>
2023-12-27 13:46:29 -0500

    commitdss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
 //import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./Home/Home" ;
+import Products from "./Products/Products";
 
 function App() {
   const openMenu = () => {
@@ -12,11 +15,11 @@ function App() {
   };
   
   return (
+    <Router>
     <div className="grid-container">
-<head>
     <link rel="stylesheet" href="style.css"></link>
   <title>Violet Cheetah</title>
-</head>
+
 <body>
           <header className="header">
             <div className="brand">
@@ -25,8 +28,8 @@ function App() {
             </div>       
 
         <div className="header-links">
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
+        <Link to="/">Home</Link>
+        <Link to="/catalog">Catalog</Link>
         </div>
 
       
@@ -49,53 +52,11 @@ function App() {
 
 
 
-  <main>
-    <div className="content">
-      <ul className="products">
-      <li> <div className="product">
-        <img className="product-image" src="images/d1.jpg" alt="product" />
-        <div className="product-name">
-            <a href="product.html">Slim Shirt</a>
-        </div>
-        <div className="product-brand">Nike</div>
-        <div className="product-price">$60</div>
-        <div className="product-rating">4.5 Stars (10 Reviews)</div>
-    </div></li>
-
-
-      <li> <div className="product">
-        <img className="product-image" src="images/d1.jpg" alt="product" />
-        <div className="product-name">
-            <a href="product.html">Slim Shirt</a>
-        </div>
-        <div className="product-brand">Nike</div>
-        <div className="product-price">$60</div>
-        <div className="product-rating">4.5 Stars (10 Reviews)</div>
-    </div></li>
-
-
-      <li> <div className="product">
-        <img className="product-image" src="images/d1.jpg" alt="product" />
-        <div className="product-name">
-            <a href="product.html">Slim Shirt</a>
-        </div>
-        <div className="product-brand">Nike</div>
-        <div className="product-price">$60</div>
-        <div className="product-rating">4.5 Stars (10 Reviews)</div>
-    </div></li>
-
-
-      <li> <div className="product">
-        <img className="product-image" src="images/d1.jpg" alt="product" />
-        <div className="product-name">
-            <a href="product.html">Slim Shirt</a>
-        </div>
-        <div className="product-brand">Nike</div>
-        <div className="product-price">$60</div>
-        <div className="product-rating">4.5 Stars (10 Reviews)</div>
-      </div></li>
-    </ul>
-  </div>
+  <main className="main">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/catalog" element ={<Products />} />
+      </Routes>
   </main>
 </div>
 
@@ -104,7 +65,7 @@ function App() {
   </footer>
 </body>
 </div>
-    
+</Router>
   );
 }
 
