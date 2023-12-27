fafc14139d6a08aafead47b2baf21fc260e5bd5a
Nikhil Mummaneni <your_email@example.com>
2023-12-26 23:56:24 -0500

    check

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,23 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const openMenu = () => {
+    document.querySelector(".sidebar")?.classList.add("open");
+  };
+  
+  const closeMenu = () => {
+    document.querySelector(".sidebar")?.classList.remove("open");
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="grid-container"></div>
   );
 }
 
