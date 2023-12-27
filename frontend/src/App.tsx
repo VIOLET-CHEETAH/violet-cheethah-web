de0cf4a39266b6bec33f6b625b903028ac6f5c87
Nikhil Mummaneni <your_email@example.com>
2023-12-27 13:25:23 -0500

    React Initial Project Structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
+//import React from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -12,8 +12,98 @@ function App() {
   };
   
   return (
-    <div className ="grid-container">
+    <div className="grid-container">
+<head>
+    <link rel="stylesheet" href="style.css"></link>
+  <title>Violet Cheetah</title>
+</head>
+<body>
+          <header className="header">
+            <div className="brand">
+              <button onClick={openMenu}>&#9776;</button>
+              <a href="index.html">Violet Cheetah</a>
+            </div>       
+
+        <div className="header-links">
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
         </div>
+
+      
+  </header>
+
+  <div>
+  <aside className="sidebar">
+    <h3>Shopping Categories</h3>
+    <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+    <ul>
+      <li>
+        <a href="index.html">Pants</a>
+      </li>
+      <li>
+        <a href="index.html">Shirts</a>
+      </li>
+    </ul>
+  </aside>
+  
+
+
+
+  <main>
+    <div className="content">
+      <ul className="products">
+      <li> <div className="product">
+        <img className="product-image" src="images/d1.jpg" alt="product" />
+        <div className="product-name">
+            <a href="product.html">Slim Shirt</a>
+        </div>
+        <div className="product-brand">Nike</div>
+        <div className="product-price">$60</div>
+        <div className="product-rating">4.5 Stars (10 Reviews)</div>
+    </div></li>
+
+
+      <li> <div className="product">
+        <img className="product-image" src="images/d1.jpg" alt="product" />
+        <div className="product-name">
+            <a href="product.html">Slim Shirt</a>
+        </div>
+        <div className="product-brand">Nike</div>
+        <div className="product-price">$60</div>
+        <div className="product-rating">4.5 Stars (10 Reviews)</div>
+    </div></li>
+
+
+      <li> <div className="product">
+        <img className="product-image" src="images/d1.jpg" alt="product" />
+        <div className="product-name">
+            <a href="product.html">Slim Shirt</a>
+        </div>
+        <div className="product-brand">Nike</div>
+        <div className="product-price">$60</div>
+        <div className="product-rating">4.5 Stars (10 Reviews)</div>
+    </div></li>
+
+
+      <li> <div className="product">
+        <img className="product-image" src="images/d1.jpg" alt="product" />
+        <div className="product-name">
+            <a href="product.html">Slim Shirt</a>
+        </div>
+        <div className="product-brand">Nike</div>
+        <div className="product-price">$60</div>
+        <div className="product-rating">4.5 Stars (10 Reviews)</div>
+      </div></li>
+    </ul>
+  </div>
+  </main>
+</div>
+
+  <footer>
+    &copy; 2023 Violet Cheetah
+  </footer>
+</body>
+</div>
     
   );
 }
