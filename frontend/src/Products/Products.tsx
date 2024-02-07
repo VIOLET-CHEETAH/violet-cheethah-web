6e2ca6f13f4b83c517245c1c20f3f6e9dd726aa6
Nikhil Mummaneni <your_email@example.com>
2024-02-07 16:16:11 -0500

    LAB 6

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -1,16 +1,42 @@
-import data from "./data";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+type Product = {
+  id: number;
+  name: string;
+  description: string;
+  brand: string; 
+  price: number;
+  rating: number;
+  numberOfReviews: number;
+  imageURL: string; 
+}
+const emptyProducts: Product[] = [];
+
 function Products() {
+  const [products, setProducts] : [Product[], (products: Product[])=>void] = useState(emptyProducts);
+
+  useEffect(()=>{
+      axios.get<Product[]>("https://localhost:7250/catalog",
+      {
+          headers: {
+              "Content-Type": "application/json",
+          },
+      })
+      .then((response)=> setProducts(response.data))
+      .catch((error)=> console.log(error));
+  }, []);
+
         return (
         <div className="content">
 
-
 <ul className="products">
-  {data.products.map((product, index) => (
-    <li key={index}>
+  {products.map((product) => (
+    <li key={product.id}>
       <div className="product">
         <img
           className="product-image"
-          src={product.imageUrl}
+          src={product.imageURL}
           alt="product"
         />
         <div className="product-name">
